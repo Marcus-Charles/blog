3158370cb84ff712c7f69d49041aae9eb58ef700
Marcus-Charles <48148519+Marcus-Charles@users.noreply.github.com>
2026-09-19 16:26:16 +0800

    post: update test

--- data/posts.js
+++ data/posts.js
@@ -23,7 +23,7 @@ window.posts = [
     "excerpt": "111",
     "cover": "",
     "readingTime": 5,
-    "content": "<h3>测试</h3>\n<p><img src=\"assets/posts/img-20260919-161553.jpg\" alt=\"OIP-C\" loading=\"lazy\"></p>"
+    "content": "<h3>测试</h3>\n<p><img src=\"assets/posts/img-20260919-161553.jpg\" alt=\"OIP-C\" loading=\"lazy\"> <img src=\"assets/posts/img-20260919-162611.jpg\" alt=\"1\" loading=\"lazy\"></p>"
   },
 {
     "id": "1",
